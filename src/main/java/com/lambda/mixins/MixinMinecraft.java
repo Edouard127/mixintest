b5f51ee97f492cb566ca046cc13865ad65e44623
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2022-05-23 15:15:54 -0400

    a

--- src/main/java/com/lambda/mixins/MixinMinecraft.java
+++ src/main/java/com/lambda/mixins/MixinMinecraft.java
@@ -1,15 +1,22 @@
 package com.lambda.mixins;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiScreen;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Minecraft.class)
-public class MixinMinecraft {
-    @Inject(method = "<clinit>", at = @At("TAIL"))
-    private static void clinit(CallbackInfo ci) {
-        System.out.println("\n\n\n\n\n\n\n\nThis got called by minecraft and is your custom mixin.\n\n\n\n\n\n\n\n");
+@Mixin(GuiScreen.class)
+public abstract class MixinMinecraft {
+    @Inject(method = "initGui", at = @At("RETURN"))
+    private void initGui(CallbackInfo ci) {
+        DefragLoadMusic.INSTANCE.initGui();
+    }
+    @Inject(method = "actionPerformed", at = @At("HEAD"))
+    public void actionPerformed(GuiButton button, CallbackInfo callbackInfo) {
+        DefragLoadMusic.INSTANCE.actionPerformed(button);
     }
 }
